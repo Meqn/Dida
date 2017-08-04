f101af9bf41c6687db21b7226617ef062bed167f
lmq <mengqing723@gmail.com>
2017-08-04 18:32:08 +0800

    gulp to wxss

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp')
+const argv = require('yargs').argv
+const connect = require('gulp-connect')
+const sass = require('gulp-sass')
+const autoprefixer = require('autoprefixer')
+const postcss = require('gulp-postcss')
+const rename = require('gulp-rename')
+
+
+
+gulp.task('sass', () => {
+	gulp.src(['src/styles/*.scss', 'src/styles/scss/*.scss'])
+	.pipe(sass().on('error', sass.logError))
+	.pipe(postcss([autoprefixer(['iOS >= 7', 'Android >= 4.1'])]))
+  .pipe(gulp.dest('src/styles/'))
+  .pipe(rename({
+    extname: '.wxss'
+  }))
+  .pipe(gulp.dest('weapp/libs/styles/'))
+	.pipe(connect.reload());
+})
+gulp.task('watch', () => {
+  gulp.watch('src/**', ['sass']);
+})
+gulp.task('server', function () {
+	argv.p = argv.p || 3016;
+	connect.server({
+		name: 'lmq',
+		root: 'src',
+		port: argv.p,
+		livereload: true
+	});
+});
+
+gulp.task('default', ['sass'], () => {
+  if (argv.w) {
+    gulp.start('watch')
+  }
+  if (argv.s) {
+    gulp.start('server')
+  }
+})
+
+
+
+
+
+
+
+
